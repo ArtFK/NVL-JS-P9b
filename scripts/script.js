e1612331f257e0faa31c92eabcb99c6b154c9f67
ArtFK <55387235+ArtFK@users.noreply.github.com>
2019-11-14 19:16:44 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var dias=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+var ciudades=["Madrid","Sevilla","Barcelona","Valencia","Zaragoza","Bilbao","Donosti"]
+
+var lunes= dias[0]+ciudades[0],
+
+document.write(lunes);
+
+
+
+
+
